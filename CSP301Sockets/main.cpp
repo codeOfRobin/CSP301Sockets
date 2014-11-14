1d7d5e13bb40b5a98a939f74d1d1b949d831f1bb
Robin Malhotra <malhotra.robin@yahoo.co.in>
2014-11-14 16:57:56 +0530

    lel

--- CSP301Sockets/main.cpp
+++ CSP301Sockets/main.cpp
@@ -1,18 +1,33 @@
-//
-//  main.cpp
-//  CSP301Sockets
-//
-//  Created by Robin Malhotra on 04/11/14.
-//  Copyright (c) 2014 Robin's code kitchen. All rights reserved.
-//
-
 #include <iostream>
+#include <boost/array.hpp>
+#include <boost/asio.hpp>
+using boost::asio::ip::tcp;
 
-int main(int argc, const char * argv[])
+int main(int argc, char* argv[])
 {
-
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    if (argc!=2)
+    {
+        std::cerr << "Usage: client <host>" << std::endl;
+        return 1;
+    }
+    boost::asio::io_service io_service;
+    tcp::resolver resolver(io_service);
+    tcp::resolver::query query(argv[1],"daytime");
+    tcp::resolver::iterator endpointIterator=resolver.resolve(query);
+    tcp::socket socket(io_service);
+    boost::asio::connect(socket, endpointIterator);
+    
+    for (; ; )
+    {
+        boost::array<char, 128> buffer;
+        boost::system::error_code error;
+        size_t len=socket.read_some(boost::asio::buffer(buffer),error);
+        if (error == boost::asio::error::eof)
+            break; // Connection closed cleanly by peer.
+        else if (error)
+            throw boost::system::system_error(error); // Some other error.
+        
+        std::cout.write(buffer.data(), len);
+    }
     return 0;
 }
-
